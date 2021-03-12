b0c95e2646620d9629f5c72f6911bc83dcfc8f67
satindar <satindarkumar@gmail.com>
2021-03-12 18:53:43 +0530

    type inference

---
+++ 002_typeinference/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+int main() {
+	
+	{
+		auto a = 10;	// a is an integer
+		auto b = 4.5;	// b is a float
+		auto c = a;		// c is an integer
+	}
+	
+	// reduce verbosity of the code.
+	{
+
+		std::vector<int> a, b;
+		std::vector<int> v{ 12,34,56,78 };
+		
+		//before c++11
+		for (std::vector<int>::iterator itr = v.begin(); itr != v.end(); itr++) {
+			a.push_back(*itr);
+		}
+
+		//c++11
+		for (auto itr = v.begin(); itr != v.end(); itr++) {
+			b.push_back(*itr);
+		}
+	}
+	return 0;
+}
